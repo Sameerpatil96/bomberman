27b6b2efdecfc7b86790a30d6cb29736633724c7
Sameerpatil96 <96463453+Sameerpatil96@users.noreply.github.com>
2022-06-08 16:32:53 +0530

    Add files via upload

---
+++ bomberman/bomberman/index.js
@@ -0,0 +1,61 @@
+const bombs = [];
+let gamePoints=0;
+let canPlay=true;
+function updateGamePoints() {
+    const gamePointsElement = document.getElementById("gamePoints");
+    gamePointsElement.innerHTML="Game Points"+gamePoints;
+}
+function addGrid(){
+    const appElement=document.getElementById('app');
+    for(let i=0;i<9;i++){
+        const row=document.createElement('div');
+        for(let j=0;j<9;j++){
+            const index=i*9+j;
+            const column=document.createElement('div');
+            column.style.display='inline-block';
+            column.style.width='60px';
+            column.style.height='60px';
+            column.style.border='1px solid black';
+            column.style.textAlign="center";
+            column.style.verticalAlign="middle";
+            column.setAttribute("index", index);
+
+               column.addEventListener("click",function() {
+                   if (canPlay) {
+                1V
+                   if (bombs.includes(index)) {
+                       column.style.backgroundColor= "red";
+                       canPlay=false;
+                   }
+                   else{
+                       column.style.background="green";
+                       gamePoints++;
+                       updateGamePoints();
+                   }
+                }
+
+               })
+            row.appendChild(column);
+        }
+        appElement.appendChild(row);
+    }
+}
+function generateBombs(){
+    while(bombs.length!==11){
+      const randomNum=Math.floor(Math.random()*100);
+      if (randomNum < 81 &&!bombs.includes(randomNum)) {
+          bombs.push(randomNum);
+      }
+    }
+}
+addGrid();
+generateBombs();
+console.log(bombs);
+// show all the bombs once a bomb is clicked 
+//give a button to start the game again
+//give a button to resert the game 
+//every colum is only clickable once 
+//if all normal cells are clicked and none is left then show alert that won
+// if a bomb is clicked then show alert u have lost 
+
+//
